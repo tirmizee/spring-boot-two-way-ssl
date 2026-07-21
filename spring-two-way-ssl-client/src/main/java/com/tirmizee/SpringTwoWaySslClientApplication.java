82845929c6727c600d89cf31e72d2c1a06893e10
pratya <pratya@cardx.co.th>
2026-07-21 22:48:59 +0700

    init

---
+++ spring-two-way-ssl-client/src/main/java/com/tirmizee/SpringTwoWaySslClientApplication.java
@@ -0,0 +1,13 @@
+package com.tirmizee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringTwoWaySslClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringTwoWaySslClientApplication.class, args);
+	}
+
+}
---
+++ spring-two-way-ssl-server/src/main/java/com/tirmizee/SpringTwoWaySslServerApplication.java
@@ -0,0 +1,13 @@
+package com.tirmizee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringTwoWaySslServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringTwoWaySslServerApplication.class, args);
+	}
+
+}
